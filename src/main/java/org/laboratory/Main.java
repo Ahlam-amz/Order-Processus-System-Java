96f3de35f0dec212d4e736c5a3bc32fe8026ff59
Ahlam-amz <a.amziane9670@uca.ac.ma>
2024-12-13 08:55:24 +0100

    adding idea

--- src/main/java/org/laboratory/Main.java
+++ src/main/java/org/laboratory/Main.java
@@ -3,7 +3,7 @@ package org.laboratory;
 public class Main {
     public static void main(String[] args) {
         String inputFilePath = "C:\\Users\\Admin\\IdeaProjects\\DevoirLibre\\src\\main\\resources\\data\\input.json";
-        String outputFilePath = "C:\\Users\\Admin\\IdeaProjects\\DevoirLibre\\src\\main\\resources\\data\\output.json";
+        String outputFilePath = "C:\\Users\\Admin\\IdeaProjects\\DevoirLibre\\src\\main\\resources\\data\\output.jsongit ";
         String errorFilePath = "C:\\Users\\Admin\\IdeaProjects\\DevoirLibre\\src\\main\\resources\\data\\error.json";
 
         // Créer une instance de OrderProcessor et démarrer le traitement périodique
